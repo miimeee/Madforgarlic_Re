563ba7fe54c242e5da790bfdf304ab05c5d8591d
miimeee <alsalsl0819@gmail.com>
2024-04-16 19:37:50 +0900

    반응형 header

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-$(function(){
-  // index.html 슬라이드 
-  $('.slide-box').slick({
+$(function () {
+  // index.html 슬라이드
+  $(".slide-box").slick({
     dots: true,
     infinite: true,
     speed: 400,
@@ -10,15 +10,27 @@ $(function(){
   });
 
   // aside tab-menu
-  $(".sub-menu-tab ul li").click(function(){
-    $(this).addClass("now")
-    console.log("킆릭")
-    $(this).siblings("li").removeClass("now")
-  })
+  $(".sub-menu-tab ul li").click(function () {
+    $(this).addClass("now");
+    console.log("킆릭");
+    $(this).siblings("li").removeClass("now");
+  });
 
   // store.html
-  $(".store-name").click(function(){
+  $(".store-name").click(function () {
     $(this).next().stop().slideToggle(300);
-    $(this).toggleClass("active")
-  })
-})
+    $(this).toggleClass("active");
+  });
+
+  // responsive
+  // Trigger
+  $(".trigger").click(function () {
+    $(this).toggleClass("active");
+    // gnb
+    $(".header-wrapper").toggleClass("active");
+  });
+  $("section, .menu a").click(function () {
+    $(".trigger").removeClass("active");
+    $(".header-wrapper").removeClass("active");
+  });
+});
