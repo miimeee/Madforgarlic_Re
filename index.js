6519ec1d9bd1990791e6de0fffdd33fe187cf281
miimeee <alsalsl0819@gmail.com>
2024-04-17 20:55:38 +0900

    brand페이지 애니메이션 추가

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 2000,
   });
+  // 스크롤 이동 시 실행되는 코드
+  $(window).scroll(function () {
+    let scrollTop = $(window).scrollTop();
+    if (scrollTop > 4500) {
+      $(".contents-container05-txt").css({
+        animation: "fadeIn 1s 0s linear both",
+      });
+    } else if (scrollTop > 1200) {
+      $(".contents-container02 > div > p").css({
+        animation: "up 0.7s 0s linear both",
+      });
+    } else if (scrollTop > 400) {
+      $(".contents-container01-txt").css({
+        animation: "up 0.5s 0s linear both",
+      });
+      $(".contents-container01-img > img").css({
+        animation: "up 0.5s 0.5s linear both",
+      });
+    } else {
+    }
+  });
   // aside tab-menu
   $(".sub-menu-tab ul li").click(function () {
     $(this).addClass("now");
